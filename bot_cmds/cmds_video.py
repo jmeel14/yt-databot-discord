9f72ef84658a8044f4ba6a44a53e3a3e86cce3fb
jmeel14 <jameelimran1@gmail.com>
2018-09-20 19:42:46 +1000

    Added signal for processing video request.

--- bot_cmds/cmds_video.py
+++ bot_cmds/cmds_video.py
@@ -34,126 +34,133 @@ def grab_vid_id(vid_link):
         return None
 
 async def cmd_func(cmd_trigger, cmd_str, msg_obj, **kwargs):
-    if len(cmd_str.split(" ")) == 2:
-        cmd_split = cmd_str.split(" ")
-        fetch_vid_id = grab_vid_id(cmd_split[1])
-        if fetch_vid_id:
-            req_API = await kwargs["self_http"].get(
-                req_build(
-                    'videos?part=snippet,statistics&id={0}'.format(fetch_vid_id),
-                    True
+    init_embed = cmd_main.Embed(
+        title = "Video Command Received",
+        description = "Your command has been received by the bot. Please wait as your request gets processed."
+    )
+    init_msg = await msg_obj.channel.send(None, embed = init_embed)
+    async with msg_obj.channel.typing():
+        if len(cmd_str.split(" ")) == 2:
+            cmd_split = cmd_str.split(" ")
+            fetch_vid_id = grab_vid_id(cmd_split[1])
+            if fetch_vid_id:
+                req_API = await kwargs["self_http"].get(
+                    req_build(
+                        'videos?part=snippet,statistics&id={0}'.format(fetch_vid_id),
+                        True
+                    )
                 )
-            )
-            if req_API.status == 200:
-                try:
-                    targ_result = loads(await req_API.text())["items"][0]
-                    desc_str = targ_result["snippet"]["description"]
-                    if len(desc_str) > 560:
-                        desc_str = desc_str[:560] + "..."
-                    output_embed = cmd_main.Embed(
-                        title = targ_result["snippet"]["title"],
-                        description = desc_str,
-                        colour = 0xDD2222
-                    )
-                    output_embed.add_field(
-                        name = "Views",
-                        value = "{:,}".format(int(targ_result["statistics"]["viewCount"]))
-                    )
-                    output_embed.add_field(
-                        name = "Likes",
-                        value = "{:,}".format(int(targ_result["statistics"]["likeCount"])),
-                        inline = False
-                    )
-                    output_embed.add_field(
-                        name = "Dislikes",
-                        value = "{:,}".format(int(targ_result["statistics"]["dislikeCount"])),
-                        inline = True
-                    )
-                    output_embed.set_image(url=targ_result["snippet"]["thumbnails"]["high"]["url"])
-
-                    req_API_channel = await kwargs["self_http"].get(
-                        req_build(
-                            'channels?part=snippet&id={0}'.format(targ_result["snippet"]["channelId"]), True
-                        )
-                    )
+                if req_API.status == 200:
                     try:
-                        channel_resp = loads(await req_API_channel.text())["items"][0]
-                        output_embed.set_footer(
-                            text = targ_result["snippet"]["channelTitle"],
-                            icon_url = channel_resp["snippet"]["thumbnails"]["default"]["url"]
+                        targ_result = loads(await req_API.text())["items"][0]
+                        desc_str = targ_result["snippet"]["description"]
+                        if len(desc_str) > 560:
+                            desc_str = desc_str[:560] + "..."
+                        output_embed = cmd_main.Embed(
+                            title = targ_result["snippet"]["title"],
+                            description = desc_str,
+                            colour = 0xDD2222
                         )
+                        output_embed.add_field(
+                            name = "Views",
+                            value = "{:,}".format(int(targ_result["statistics"]["viewCount"]))
+                        )
+                        output_embed.add_field(
+                            name = "Likes",
+                            value = "{:,}".format(int(targ_result["statistics"]["likeCount"])),
+                            inline = False
+                        )
+                        output_embed.add_field(
+                            name = "Dislikes",
+                            value = "{:,}".format(int(targ_result["statistics"]["dislikeCount"])),
+                            inline = True
+                        )
+                        output_embed.set_image(url=targ_result["snippet"]["thumbnails"]["high"]["url"])
+
+                        req_API_channel = await kwargs["self_http"].get(
+                            req_build(
+                                'channels?part=snippet&id={0}'.format(targ_result["snippet"]["channelId"]), True
+                            )
+                        )
+                        try:
+                            channel_resp = loads(await req_API_channel.text())["items"][0]
+                            output_embed.set_footer(
+                                text = targ_result["snippet"]["channelTitle"],
+                                icon_url = channel_resp["snippet"]["thumbnails"]["default"]["url"]
+                            )
+                        except:
+                            output_embed.set_footer(
+                                text = targ_result["snippet"]["channelTitle"],
+                                icon_url = "https://i.imgur.com/YxrOhoy.pngg"
+                            )
                     except:
-                        output_embed.set_footer(
-                            text = targ_result["snippet"]["channelTitle"],
-                            icon_url = "https://i.imgur.com/YxrOhoy.pngg"
+                        output_embed = cmd_main.err_embed(
+                            "Video Fetch Failure",
+                            "An error occurred attempting to get your requested video. The video may have been deleted, or the video ID is wrong.",
+                            "API Response - Page Not Found"
                         )
-                except:
+                else:
                     output_embed = cmd_main.err_embed(
                         "Video Fetch Failure",
-                        "An error occurred attempting to get your requested video. The video may have been deleted, or the video ID is wrong.",
+                        "Your request could not be accepted. Please try again later.",
                         "API Response - Page Not Found"
                     )
             else:
                 output_embed = cmd_main.err_embed(
                     "Video Fetch Failure",
-                    "Your request could not be accepted. Please try again later.",
-                    "API Response - Page Not Found"
+                    "The URL that you provided could not be used to send a request. Please check to make sure the URL is correct and try again.",
+                    "Malformed video URL error"
                 )
-        else:
-            output_embed = cmd_main.err_embed(
-                "Video Fetch Failure",
-                "The URL that you provided could not be used to send a request. Please check to make sure the URL is correct and try again.",
-                "Malformed video URL error"
-            )
-    elif len(cmd_str.split(" ")) > 2:
-        cmd_args = cmd_str.split(" ")
-        if cmd_args[1] == "tags":
-            fetch_vid_id = grab_vid_id(cmd_args[2])
-            if fetch_vid_id:
-                req_API = await kwargs["self_http"].get(
-                    req_build(
-                        'videos?part=snippet&id={0}'.format(fetch_vid_id),
-                        True
-                    )
-                )
-                try:
-                    targ_result = loads(await req_API.text())["items"][0]["snippet"]
-                    output_embed = cmd_main.Embed(
-                        title = targ_result["title"],
-                        description = "**The video you requested had the following tags:**\n{0}".format(
-                            "\n".join(targ_result["tags"][:20])
-                        ),
-                        colour = 0xDD2222
-                    )
-                    output_embed.set_thumbnail(url = targ_result["thumbnails"]["default"]["url"])
-
-                    req_API_channel = await kwargs["self_http"].get(
+        elif len(cmd_str.split(" ")) > 2:
+            cmd_args = cmd_str.split(" ")
+            if cmd_args[1] == "tags":
+                fetch_vid_id = grab_vid_id(cmd_args[2])
+                if fetch_vid_id:
+                    req_API = await kwargs["self_http"].get(
                         req_build(
-                            'channels?part=snippet&id={0}'.format(targ_result["channelId"]), True
+                            'videos?part=snippet&id={0}'.format(fetch_vid_id),
+                            True
                         )
                     )
-                    channel_resp = loads(await req_API_channel.text())["items"][0]
                     try:
-                        output_embed.set_footer(
-                            text = targ_result["channelTitle"],
-                            icon_url = channel_resp["snippet"]["thumbnails"]["default"]["url"]
+                        targ_result = loads(await req_API.text())["items"][0]["snippet"]
+                        output_embed = cmd_main.Embed(
+                            title = targ_result["title"],
+                            description = "**The video you requested had the following tags:**\n{0}".format(
+                                "\n".join(targ_result["tags"][:20])
+                            ),
+                            colour = 0xDD2222
                         )
-                    except:
-                        output_embed.set_footer(
-                            text = targ_result["channelTitle"],
-                            icon_url = "https://i.imgur.com/YxrOhoy.png"
+                        output_embed.set_thumbnail(url = targ_result["thumbnails"]["default"]["url"])
+
+                        req_API_channel = await kwargs["self_http"].get(
+                            req_build(
+                                'channels?part=snippet&id={0}'.format(targ_result["channelId"]), True
+                            )
                         )
-                except Exception as TagsFetchError:
-                    output_embed = cmd_main.err_embed(
-                        "Video Tags Error",
-                        "An error occurred attempting to get the tags of your requested video. The video may have been deleted, or the video ID is wrong.",
-                        "Invalid video ID error"
-                    )
-                    print(TagsFetchError)
-    return {
-        "output_msg": await msg_obj.channel.send(None, embed = output_embed),
-        "output_admin": False
-    }
+                        channel_resp = loads(await req_API_channel.text())["items"][0]
+                        try:
+                            output_embed.set_footer(
+                                text = targ_result["channelTitle"],
+                                icon_url = channel_resp["snippet"]["thumbnails"]["default"]["url"]
+                            )
+                        except:
+                            output_embed.set_footer(
+                                text = targ_result["channelTitle"],
+                                icon_url = "https://i.imgur.com/YxrOhoy.png"
+                            )
+                    except Exception as TagsFetchError:
+                        output_embed = cmd_main.err_embed(
+                            "Video Tags Error",
+                            "An error occurred attempting to get the tags of your requested video. The video may have been deleted, or the video ID is wrong.",
+                            "Invalid video ID error"
+                        )
+                        print(TagsFetchError)
+        await init_msg.delete()
+        return {
+            "output_msg": await msg_obj.channel.send(None, embed = output_embed),
+            "output_admin": False
+        }
 
 cmd_video = cmd_main.Command(
     "Video",
